354805e3da02733bea321cddd1435b77860a9d22
Asianadian <danielchen.mc@gmail.com>
2024-07-29 20:22:45 -0400

    Created endpoint for FDC food information

--- pages/api/food/search/fdc/routes.ts
+++ pages/api/food/search/fdc/routes.ts
@@ -1,11 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-import { searchFood } from "@/pages/api/food/search/utils/search_utils"
+import { searchFood } from "@/pages/api/food/search/fdc/utils/search_utils"
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  res.setHeader('Access-Control-Allow-Headers', 'x-api-key');
+  
   if (req.method === "POST") {
     return await searchFood(req, res)
   }
