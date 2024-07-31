3fff5681cf38c7c67dfd00fe99939138d5ff0f66
Daniel Chen <145870392+dym-chen@users.noreply.github.com>
2024-07-30 22:51:00 -0400

    creating the ui for the profile page (#13)

--- app/home/profile/page.tsx
+++ app/home/profile/page.tsx
@@ -11,7 +11,42 @@ export default function Home() {
         secondary="/home/profile"
         secondaryTabs={HOME_SECONDARY_TABS}
       />
-      <main></main>
+      <main>
+        <div className="w-full inline-block">
+          <div className="ml-[15%] w-[60%]">
+            {/* name title */}
+            <h1 className="text-[20px] font-semibold mb-[20px]">
+              danielymchen&apos;s profile
+            </h1>
+
+            {/* profile photo section */}
+            <div className="px-[20px] pb-[20px] pt-[30px] bg-slate-300 justify-between flex text-black">
+              <div className="w-auto">
+                <div className="w-[250px] h-[250px] bg-gray-500"></div>
+              </div>
+              <div className="w-[65%] flex flex-col  gap-[8px]">
+                <h2 className="text-[17px] font-semibold">danielymchen</h2>
+                <p>19 years old</p>
+                <p>Male</p>
+                <p>Member since May 8, 2023</p>
+
+                <button
+                  className="btn btn-primary w-auto mr-auto "
+                  type="button"
+                >
+                  EDIT PROFILE
+                </button>
+                <button
+                  className="btn btn-primary w-auto mr-auto"
+                  type="button"
+                >
+                  EDIT PHOTOS
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
